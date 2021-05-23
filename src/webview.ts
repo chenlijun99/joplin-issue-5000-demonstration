f0879c48fde3852dfa65e0379f5c538b6735ca93
Chen Lijun <chenlijun1999@gmail.com>
2021-05-23 17:58:28 +0200

    Bug reproduction

--- src/webview.ts
+++ src/webview.ts
@@ -1,9 +1,13 @@
 document.addEventListener('click', event => {
 	const element = event.target;
+	// @ts-ignore
 	if (element.className === 'toc-item-link') {
+		// @ts-ignore
 		console.debug('TOC Plugin Webview: Sending scrollToHash message', element.dataset.slug);
+		// @ts-ignore
 		webviewApi.postMessage({
 			name: 'scrollToHash',
+			// @ts-ignore
 			hash: element.dataset.slug,
 		});
 	}
